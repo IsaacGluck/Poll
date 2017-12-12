a76759d1925a432d84146175c5f5fefb8c94d77c
andersbando-hess <bandersman@gmail.com>
2017-12-12 21:13:18 +0100

    Reformatted menu drawer and added about button

--- app/src/main/java/hu/ait/android/poll/PollActivity.java
+++ app/src/main/java/hu/ait/android/poll/PollActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -31,6 +32,8 @@ public class PollActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
 
     private PollAdapter adapter;
+    private DrawerLayout drawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +51,7 @@ public class PollActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
@@ -120,13 +123,29 @@ public class PollActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.nav_logout) {
-            FirebaseAuth.getInstance().signOut();
-            finish();
+        switch (id) {
+            case R.id.nav_logout:
+                FirebaseAuth.getInstance().signOut();
+                finish();
+            case R.id.nav_about:
+                showSnackBarMessage(getString(R.string.txt_about));
         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void showSnackBarMessage(String message) {
+        Snackbar.make(drawer,
+                message,
+                Snackbar.LENGTH_LONG
+        ).setAction(R.string.Hide, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //...
+            }
+        }).show();
     }
 }
